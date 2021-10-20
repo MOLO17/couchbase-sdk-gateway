7b9f69778f2093b5753895bb764e77afa24d5c03
mattia.pettenuzzo <mattia.pettenuzzo@molo17.com>
2021-10-20 16:32:46 +0200

    chore: add console log

--- src/routers/queryRouter.js
+++ src/routers/queryRouter.js
@@ -1,28 +1,31 @@
-const connection = require("../shared/couchbaseConnection");
-const Router = require("koa-router");
+const connection = require('../shared/couchbaseConnection');
+const Router = require('koa-router');
 
-module.exports = new Router().post("/", async (ctx, next) => {
+module.exports = new Router().post('/', async (ctx, next) => {
   try {
-    const { query = "", options = {} } = ctx.request.body;
+    const { query = '', options = {} } = ctx.request.body;
 
     if (!query) {
       ctx.body = {
-        error: "missing_query_parameter",
-        message: "Missing Query Parameter",
+        error: 'missing_query_parameter',
+        message: 'Missing Query Parameter',
       };
       ctx.status = 400;
       return next();
     }
 
-    const queryString = query.split("$bucket").join(`\`${connection.bucketName}\``);
+    const queryString = query
+      .split('$bucket')
+      .join(`\`${connection.bucketName}\``);
 
     console.log(`Query ${queryString}`);
     console.log(` WITH -> ${JSON.stringify(options)}`);
 
-    const result = await connection.query(
-      queryString,
-      options
-    );
+    const t1 = Date.now();
+
+    const result = await connection.query(queryString, options);
+
+    console.log('FETCH EXECUTION TIME:', Date.now() - t1);
 
     ctx.status = 200;
     ctx.body = result;
