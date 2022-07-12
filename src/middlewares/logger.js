0f5843c79e31a79260e22314abef8f4f7bda141f
Alex Mazzariol <alex.mazzariol@molo17.com>
2022-07-12 08:58:12 +0200

    chore: avoid useless /health log entries

--- src/middlewares/logger.js
+++ src/middlewares/logger.js
@@ -5,14 +5,16 @@ const logger = () => async (ctx, next) => {
 
   await next();
 
-  process.stdout.write(
-    chalk.greenBright("\nTimestamp: ") +
-      chalk.green(now.toISOString()) +
-      chalk.yellowBright("\nResponse time: ") +
-      chalk.yellow(`${Date.now() - now.getTime()}ms`) +
-      chalk.magentaBright("\nRequest path: ") +
-      chalk.magenta(`${ctx.request.method}: ${ctx.request.url}\n`)
-  );
+  if(ctx.request.url !== '/health') {
+    process.stdout.write(
+      chalk.greenBright("\nTimestamp: ") +
+        chalk.green(now.toISOString()) +
+        chalk.yellowBright("\nResponse time: ") +
+        chalk.yellow(`${Date.now() - now.getTime()}ms`) +
+        chalk.magentaBright("\nRequest path: ") +
+        chalk.magenta(`${ctx.request.method}: ${ctx.request.url}\n`)
+    );
+  }
 };
 
 module.exports = logger;
